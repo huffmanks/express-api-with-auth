03f0ce285344ff4f091c8b762738ff62d7d2e09c
huffmanks <huffmanks2@gmail.com>
2022-09-03 17:20:56 -0400

    jwt and envs

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,17 +3,16 @@ import jwt from 'jsonwebtoken'
 import config from '../config'
 
 export function signJwt(payload: Object, keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey', options?: jwt.SignOptions | undefined) {
-    const signingKey = Buffer.from(config[keyName], 'base64').toString('ascii')
+    const signingKey = Buffer.from(config[keyName]!, 'base64').toString('ascii')
 
     return jwt.sign(payload, signingKey, {
         ...(options && options),
         // algorithm: 'RS256',
-        algorithm: 'HS256',
     })
 }
 
 export function verifyJwt<T>(token: string, keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey'): T | null {
-    const publicKey = Buffer.from(config[keyName], 'base64').toString('ascii')
+    const publicKey = Buffer.from(config[keyName]!, 'base64').toString('ascii')
 
     try {
         const decoded = jwt.verify(token, publicKey) as T
